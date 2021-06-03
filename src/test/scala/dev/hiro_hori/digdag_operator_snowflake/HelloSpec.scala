936f8fca86ded1b2b598333072aa3aa1ba4e1ac9
hori <h_hori@septeni-original.co.jp>
2021-06-03 19:32:07 +0900

    へいへい

--- src/test/scala/dev/hiro_hori/digdag_operator_snowflake/HelloSpec.scala
+++ src/test/scala/dev/hiro_hori/digdag_operator_snowflake/HelloSpec.scala
@@ -3,8 +3,8 @@ package dev.hiro_hori.digdag_operator_snowflake
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class HelloSpec extends AnyFlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
-  }
-}
+//class HelloSpec extends AnyFlatSpec with Matchers {
+//  "The Hello object" should "say hello" in {
+//    Hello.greeting shouldEqual "hello"
+//  }
+//}
