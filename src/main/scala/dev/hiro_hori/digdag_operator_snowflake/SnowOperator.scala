70f23dba3ccd9aec9f6322a4957cc7d63aed9fed
hori <h_hori@septeni-original.co.jp>
2021-06-14 13:28:44 +0900

    db, schema, warehouse, roleをexportedなparameterか、オペレータのパラメータから取るように

--- src/main/scala/dev/hiro_hori/digdag_operator_snowflake/SnowOperator.scala
+++ src/main/scala/dev/hiro_hori/digdag_operator_snowflake/SnowOperator.scala
@@ -5,11 +5,16 @@ import io.digdag.spi.{Operator, OperatorContext, OperatorFactory, TaskResult}
 import io.digdag.util.BaseOperator
 import org.slf4j.LoggerFactory
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.common.base.Optional
+import io.digdag.client.config.Config
 import net.snowflake.client.jdbc.SnowflakeDriver
+
+import java.util.Properties
 
 //import java.net.URL
 import java.sql.{Connection, DriverManager}
 import scala.io.Source
+import scala.jdk.OptionConverters._
 
 
 // オペレータ本体
@@ -22,34 +27,64 @@ class SnowOperator(_context: OperatorContext) extends BaseOperator(_context) {
     // Configの内容をJSONのPrettyPrintで出力
     val pretty = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(config)
     logger.debug(pretty)
-    logger.debug("key = {}", config.get("key", classOf[String]))
-    logger.debug("array = {}", config.getList("array", classOf[Int]).asScala.toSeq)
-    val password = this.context.getSecrets.getSecret("snow.password")
-    logger.debug("snow.password = {}", password)
 
     val source = Source.fromFile(config.get("_command", classOf[String]))
     val sql = source.mkString
     source.close()
     logger.info(sql)
 
-    val snowConfigs = config.getMap("snow", classOf[String], classOf[String])
     val conn = getConnection(
-      snowConfigs.asScala("host"),
-      snowConfigs.asScala("user"),
-      this.context.getSecrets.getSecret("snow.password")
+      getConfigFromOperatorParameterOrExportedParameter(config,"host"),
+      getConfigFromOperatorParameterOrExportedParameter(config,"user"),
+      this.context.getSecrets.getSecret("snow.password"),
+      getConfigFromOperatorParameterOrExportedParameterOptional(config, "db"),
+      getConfigFromOperatorParameterOrExportedParameterOptional(config, "schema"),
+      getConfigFromOperatorParameterOrExportedParameterOptional(config, "warehouse"),
+      getConfigFromOperatorParameterOrExportedParameterOptional(config, "role"),
     )
     val stmt = conn.createStatement()
     stmt.execute(sql)
+    conn.close()
     // オペレータの処理が無事成功した場合はTaskResultを返す
     TaskResult.empty(this.request)
   }
 
-  def getConnection(host: String, user: String, password: String): Connection = {
+  def getConnection(
+                     host: String,
+                     user: String,
+                     password: String,
+                     db: Option[String],
+                     schema: Option[String],
+                     warehouse: Option[String],
+                     role: Option[String]
+  ): Connection = {
     DriverManager.registerDriver(
-//    Class.forName("net.snowflake.client.jdbc.SnowflakeDriver").newInstance()
       new SnowflakeDriver()
     )
-    DriverManager.getConnection(s"jdbc:snowflake://${host}/", user, password)
+
+    val prop = new Properties()
+    prop.put("user", user)
+    prop.put("password", password)
+    db.foreach(x => prop.put("db", x))
+    schema.foreach(x => prop.put("schema", x))
+    warehouse.foreach(x => prop.put("warehouse", x))
+    role.foreach(x => prop.put("role", x))
+    DriverManager.getConnection(s"jdbc:snowflake://${host}", prop)
+  }
+
+  def getConfigFromOperatorParameterOrExportedParameter(config: Config, configName: String): String =
+    Option(config.getOptional(configName, classOf[String]).orNull())
+      .getOrElse(config.getNested("snow").get(configName, classOf[String]))
+
+  def getConfigFromOperatorParameterOrExportedParameterOptional(config: Config, configName: String): Option[String] = {
+
+    val o0: Option[String] = Option(config.getOptional(configName, classOf[String]).orNull())
+    val o1: Option[String] = Option(config.getNested("snow").getOptional(configName, classOf[String]).orNull())
+    if (o0.isDefined) {
+      o0
+    } else {
+      o1
+    }
   }
 }
 
